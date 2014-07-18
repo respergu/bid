2f1edd9729ed4c3f9709e684cd84c824f9c08985
respergu <respergu@gmail.com>
2014-07-18 11:59:36 -0700

    initial

---
+++ src/main/java/net/eim/bidprocessor/config/AppConfig.java
@@ -0,0 +1,9 @@
+package net.eim.bidprocessor.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "net.eim.bidprocessor")
+class AppConfig {
+}
